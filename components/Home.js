66f79da4ec58c4b2fe214fce37764b35aa573e1a
greenHound <rv.rishabhverma1996@gmail.com>
2022-02-25 12:51:58 +0530

    favicon

--- components/Home.js
+++ components/Home.js
@@ -47,7 +47,7 @@ export default function HomePage({ list }) {
           crossOrigin="anonymous"
         />
         
-        {/* <link id="favicon" rel="shortcut icon" type="image/png" href={'/logo.png'} /> */}
+        <link id="favicon" rel="shortcut icon" type="image/png" href={'/android-icon-36x36.png'} />
       </Head>
 
       <header className={Styles.headerContainer}>
